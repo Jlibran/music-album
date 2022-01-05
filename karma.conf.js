09701f28444a3e2f83ebdd3c92cc9ffbed3ab15c
Josef <Josef.libran-pierre@delta.com>
2022-01-05 11:33:01 -0500

    adding files

--- karma.conf.js
+++ karma.conf.js
@@ -1,44 +1,58 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/0.13/config/configuration-file.html
+
+const argv = require('minimist')(process.argv.slice(2)),
+  part = (argv.part !== true) && argv.part;
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage-istanbul-reporter'),
+      require('karma-spec-reporter'),
+      require('@angular/cli/plugins/karma')
     ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
+    client:{
+      args: [part],
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      './src/test.ts': ['@angular/cli']
     },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/music-album'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+    mime: {
+      'text/x-typescript': ['ts','tsx']
     },
-    reporters: ['progress', 'kjhtml'],
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: config.angularCli && config.angularCli.codeCoverage
+              ? ['progress', 'coverage-istanbul']
+              : ['spec'],
+    specReporter: {
+      maxLogLines: 1,
+      suppressErrorSummary: false,
+      suppressFailed: false,
+      suppressPassed: false,
+      suppressSkipped: false,
+      showSpecTiming: true,
+      failFast: false
+    },           
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: false
   });
 };
---
+++ src/app/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Album } from './album';
+import { Product } from './product';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private _albumUrl:'../assets/album.json';
+  private _productsUrl: '../assets/products.json'
+
+  constructor(private _http: HttpClient) { }
+  
+  getAlbum(id:number):Observable<Album>{
+  return this._http.get(this._albumUrl).pipe
+  (map(response => (<Album>response)));
+  }
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).pipe
+    (map(response => <Product[]>response));
+    }
+}
